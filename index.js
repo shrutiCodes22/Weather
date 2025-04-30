4312d8fa44464b03659fe3590500626f35d60c3a
shrutiCodes22 <shruti22052004@gmail.com>
2025-04-30 15:15:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ async function fetchUserWeatherInfo(coordinates){
 
     //API CALL
     try{
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric
       `); 
       const data = await response.json();
       loadingScreen.classList.remove("active");
